e9c789a8600d3a1164e2666af8f3171126bf00a7
Richard Ruppel <richard.ruppel001@stud.fh-dortmund.de>
2024-06-27 11:40:13 +0200

    init

---
+++ backend/src/main/java/de/fhdortmund/blutentnahmebackend/JacksonConfig.java
@@ -0,0 +1,19 @@
+package de.fhdortmund.blutentnahmebackend;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
+    }
+}
+
