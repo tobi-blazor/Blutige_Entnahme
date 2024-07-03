62aebe650a1b0c2b0072ab28673c3074354ef932
MrGinn <Ginn9060@gmail.com>
2024-07-03 13:26:57 +0200

    added Button + behaviour
    added Button
    added PressOn
    -alert with message 'Sie werden nun abgemeldet'
    -navigation to 'home'

--- BlutentnahmeReactApp/src/screens/Profil.tsx
+++ BlutentnahmeReactApp/src/screens/Profil.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet, Image} from "react-native";
+import { View, Text, StyleSheet, Image, Button, Alert} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types/navigation";
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Home"
+>;
 
 function Profil() {
+  const navigation = useNavigation<HomeScreenNavigationProp>();
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={{uri: 'https://cdn.pixabay.com/photo/2017/08/17/10/47/paris-2650808_960_720.jpg'}} />
       <Text style={styles.text}>Profil</Text>
+      <Button
+        title="Abmelden"
+        onPress={() => {Alert.alert('Sie werden nun abgemeldet');  navigation.navigate("Home")}}
+      />
     </View>
   );
 }
