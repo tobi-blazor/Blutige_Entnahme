1cccab403e7db16b316f187fbcf99264a8da8603
Tobias Blazor <tobias1153@gmail.com>
2024-07-18 21:56:41 +0200

    person vererbung

---
+++ BlutentnahmeAPI/BlutentnahmeAPI/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlutentnahmeAPI.Models
+{
+    public class Person
+    {
+        [Key]
+        public string PersonID { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+    }
+}
