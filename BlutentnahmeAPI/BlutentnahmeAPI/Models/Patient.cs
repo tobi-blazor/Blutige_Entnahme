5a983520c23d43433bb55f1d43e20757b3c3d8a2
Tobias Blazor <tobias1153@gmail.com>
2024-07-18 21:56:41 +0200

    person vererbung

--- BlutentnahmeAPI/BlutentnahmeAPI/Models/Patient.cs
+++ BlutentnahmeAPI/BlutentnahmeAPI/Models/Patient.cs
@@ -2,13 +2,8 @@
 
 namespace BlutentnahmeAPI.Models
 {
-    public class Patient
+    public class Patient : Person
     {
-        [Key]
-        public string PersonID { get; set; }
-        public string Vorname { get; set; }
-        public string Nachname { get; set; }
-        public DateTime Geburtsdatum { get; set; }
         public string Hinweise { get; set; }
         public ICollection<Auftrag> Aufträge { get; } = new List<Auftrag>();
     }
