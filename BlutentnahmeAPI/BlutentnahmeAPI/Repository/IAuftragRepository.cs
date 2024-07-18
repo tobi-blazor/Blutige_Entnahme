c8d352385fc01aca49898a651b59b84d731698cd
Tobias Blazor <tobias1153@gmail.com>
2024-07-18 19:13:07 +0200

    Aufträge Repository

---
+++ BlutentnahmeAPI/BlutentnahmeAPI/Repository/IAuftragRepository.cs
@@ -0,0 +1,14 @@
+﻿using BlutentnahmeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BlutentnahmeAPI.Repository
+{
+    public interface IAuftragRepository
+    {
+        Task<IEnumerable<Auftrag>> GetAufträgeAsync();
+        Task<Auftrag> GetAuftragAsync(string id);
+        Task<IEnumerable<Auftrag>> GetAktiveAufträgeAsync();
+        Task<Auftrag?> GetAktivenAuftragWithNichtEntnommeneBlutprobenAsync(string id);
+    }
+}
