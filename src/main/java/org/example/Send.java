d40ec08cdce27ac13a44572f7787d5ddef15eec9
ripe-1 <ductienvu2003@gmail.com>
2024-07-20 21:23:26 +0200

    Tupel verbessert

--- src/main/java/org/example/Send.java
+++ src/main/java/org/example/Send.java
@@ -27,9 +27,9 @@ public class Send {
             channel.basicPublish("", QUEUE_NAME, null, patientMessage.getBytes(StandardCharsets.UTF_8));
             System.out.println("Patient: '" + patientMessage + "' was sent successfully");
 
-            /*// Example of sending an auftrag message
-            String auftragsID = "AUF001";
-            String geplanterZeitpunkt = "2024-01-01 10:00:00";
+            // Example of sending an auftrag message
+            String auftragsID = "AUF006";
+            String geplanterZeitpunkt = "2024-07-25 10:00:00";
             String patientPersonID = personID;
 
             String auftragMessage = "AUFTRAG:" + auftragsID + "," + geplanterZeitpunkt + "," + patientPersonID;
@@ -37,26 +37,28 @@ public class Send {
             System.out.println("Auftrag: '" + auftragMessage + "' was sent successfully");
 
             // Example of sending a blutprobe message
-            String rohrID = "ROHR001";
-            String spaetesterEntnahmezeitpunkt = "2024-01-01 12:00:00";
+            String rohrID = "BPR0020";
+            String spaetesterEntnahmezeitpunkt = "2024-11-15 12:00:00";
             String grund = "Routine Check";
+            String hinweiseFuerBlutprobe = "Super Wichtig";
             String entnahmeZeitpunkt = "2024-01-01 11:00:00";
             String personalPersonID = "PER001";
-            String laborEingang = "2024-01-01 13:00:00";
+            String auftragsIDFuerBlutprobe = auftragsID;
+            String laborEingang = "2024-07-05 13:00:00";
 
-            String blutprobeMessage = "BLUTPROBE:" + rohrID + "," + spaetesterEntnahmezeitpunkt + "," + grund + "," + hinweise + "," + entnahmeZeitpunkt + "," + personalPersonID + "," + auftragsID + "," + laborEingang;
+            String blutprobeMessage = "BLUTPROBE:" + rohrID + "," + spaetesterEntnahmezeitpunkt + "," + grund + "," + hinweiseFuerBlutprobe + "," + entnahmeZeitpunkt + "," + personalPersonID + "," + auftragsIDFuerBlutprobe + "," + laborEingang;
             channel.basicPublish("", QUEUE_NAME, null, blutprobeMessage.getBytes(StandardCharsets.UTF_8));
             System.out.println("Blutprobe: '" + blutprobeMessage + "' was sent successfully");
 
             // Example of sending a personal message
-            String personalID = "PER001";
+            String personIDfFuerPersonal = "PER006";
             String personalVorname = "Anna";
             String personalNachname = "Musterfrau";
             String personalGeburtsdatum = "1975-05-05 00:00:00";
 
-            String personalMessage = "PERSONAL:" + personalID + "," + personalVorname + "," + personalNachname + "," + personalGeburtsdatum;
+            String personalMessage = "PERSONAL:" + personIDfFuerPersonal + "," + personalVorname + "," + personalNachname + "," + personalGeburtsdatum;
             channel.basicPublish("", QUEUE_NAME, null, personalMessage.getBytes(StandardCharsets.UTF_8));
-            System.out.println("Personal: '" + personalMessage + "' was sent successfully");*/
+            System.out.println("Personal: '" + personalMessage + "' was sent successfully");
         }
     }
 }
